2d601a19ea9a57d99a733bd0931faaddadf2f4b5
Geovanna Nunes <geovannanunes349@gmail.com>
2026-02-26 14:55:56 -0300

    Add files via upload

---
+++ Aulas/Algoritmos-tecnicas-programacao-I/primeiro-codigo-c/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main()
+
+{
+    //Alocação de Memoria
+    int a;
+    int b;
+    int c;
+
+    //Entrada de Dados
+    a=10;
+    b=5;
+
+    //Processamento Calculo
+    c=a+b;
+    //Saida de Dados
+    printf("O resultado da soma é: %i", c);
+}
